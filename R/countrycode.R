e59580ac8a2ebd9aee59b7517c64451d4ed3e912
Enrico Spinielli <espinielli@users.noreply.github.com>
2016-05-31 16:57:23 +0200

    fully quote package

--- R/countrycode.R
+++ R/countrycode.R
@@ -35,8 +35,8 @@
 countrycode <-
   function (sourcevar, origin, destination, warn = FALSE) {
     # Sanity check
-    origin_codes <- names(countrycode_data)[!(
-      names(countrycode_data) %in% c(
+    origin_codes <- names(countrycode::countrycode_data)[!(
+      names(countrycode::countrycode_data) %in% c(
         "continent",
         "region",
         "regex",
@@ -48,7 +48,7 @@ countrycode <-
       )
     )]
     destination_codes <-
-      names(countrycode_data)[!(names(countrycode_data) %in% c("regex"))]
+      names(countrycode::countrycode_data)[!(names(countrycode::countrycode_data) %in% c("regex"))]
     if (!origin %in% origin_codes) {
       stop("Origin code not supported")
     }
@@ -56,9 +56,9 @@ countrycode <-
       stop("Destination code not supported")
     }
     if (origin == 'country.name') {
-      dict = na.omit(countrycode_data[, c('regex', destination)])
+      dict = na.omit(countrycode::countrycode_data[, c('regex', destination)])
     } else{
-      dict = na.omit(countrycode_data[, c(origin, destination)])
+      dict = na.omit(countrycode::countrycode_data[, c(origin, destination)])
     }
     # Prepare output vector
     destination_vector <- rep(NA, length(sourcevar))
