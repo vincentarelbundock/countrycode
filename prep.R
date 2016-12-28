453b4b8cd407a7f291fdc7d74ed10068b0485600
CJ Yetman <cj@cjyetman.com>
2016-12-28 20:21:07 +0100

    add an origin_regex parameter for custom dictionary use

--- prep.R
+++ prep.R
@@ -2,7 +2,6 @@ library(devtools)
 document()
 
 countrycode_data = read.csv('data/countrycode_data.csv', stringsAsFactors=FALSE, na.strings='')
-colnames(countrycode_data)[1] = 'country.name'
 countrycode_data$iso2c[countrycode_data$country.name=='Namibia'] = 'NA'
-countrycode_data$regex = iconv(countrycode_data$regex, to='UTF-8')
+countrycode_data$country.name.regex = iconv(countrycode_data$country.name.regex, to='UTF-8')
 save(countrycode_data, file='data/countrycode_data.rda')
