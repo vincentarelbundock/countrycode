c553195d5bc332cc4273b76199727345f86c85fb
Vincent Arel-Bundock <varel@umich.edu>
2017-02-16 07:57:14 -0500

    test: sentence with multiple matches returns NA

--- tests/testthat/test-regex-sentence.R
+++ tests/testthat/test-regex-sentence.R
@@ -9,3 +9,11 @@ problems = origin[is.na(target)]
 test_that('countrycode can catch countries in the middle of a sentence', {
     expect_equal(length(problems), 0)
 })
+
+test_that('sentence with two matches returns NA',{
+    expect_equal(countrycode('report from Canada and the United States.', 'country.name', 'country.name'), NA_character_)
+})
+
+test_that('sentence with a single match',{
+    expect_equal(countrycode('report from Canada.', 'country.name', 'country.name'), 'Canada')
+})
