e1cf0a32f1fab0dbfc1a5183da65f8d2cba24d12
Vincent Arel-Bundock <varel@umich.edu>
2019-01-14 10:28:08 -0500

    vdem czechoslovakia

--- dictionary/build_panel.R
+++ dictionary/build_panel.R
@@ -35,11 +35,15 @@ unlink(tmp)
 
 # V-Dem
 vdem = readRDS('dictionary/data_vdem_v8_april2018.rds') %>%
+       # TODO: Improve regex
        dplyr::filter(country_name != 'Democratic Republic of Vietnam',
                      country_name != 'Republic of Vietnam') %>%
+       # TODO: Czechoslovakia vs. Czech Republic vs. Czechia
+       dplyr::mutate(country_name = ifelse((country_name == 'Czech Republic') & (year < 1993), 
+                                        'Czechoslovakia', country_name)) %>% 
+       # convert country names
        dplyr::mutate(country.name.en.regex = CountryToRegex(country_name)) %>%
        dplyr::select(country.name.en.regex, vdem.name = country_name, vdem = country_id, year)
-
 
 # Hack to artificially extend the temporal coverage of panel datasets using the
 # last available year
