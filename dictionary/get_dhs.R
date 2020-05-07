376c06c8cc6918559c781b81a815b57b69dcc93e
mcooper <mw.coop.r@gmail.com>
2020-05-07 13:32:05 -0400

    Filter out code for Ondo state Nigeria

--- dictionary/get_dhs.R
+++ dictionary/get_dhs.R
@@ -3,6 +3,7 @@ get_dhs = function() {
     dhs = jsonlite::fromJSON(url)[['Data']] %>%
         dplyr::select(CountryName, DHS_CountryCode) %>%
         dplyr::mutate(country.name.en.regex = CountryToRegex(CountryName)) %>%
-        dplyr::select(-CountryName)
+        dplyr::select(-CountryName) %>%
+        dplyr::filter(DHS_CountryCode != 'OS')
     return(dhs)
 }
