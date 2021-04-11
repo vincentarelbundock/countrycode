c64acadc9c821f3a0c4733b1b5eb2d295f6d58cc
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-04-11 19:05:34 -0400

    vietnam regex improvement

--- tests/testthat/test-corner-cases.R
+++ tests/testthat/test-corner-cases.R
@@ -7,3 +7,13 @@ test_that('Namibia iso2c is not converted to NA', {
     expect_equal('NA', countrycode("Namibia", 'country.name', 'wb_api2c'))
     expect_equal('NA', countrycode("Namibia", 'country.name', 'ecb'))
 })
+
+test_that("Viet Nam variations", {
+  expect_equal("RVN", countrycode("Republic of Viet Nam", "country.name", "cowc"))
+  expect_equal("RVN", countrycode("Republic of VietNam", "country.name", "cowc"))
+  expect_equal("RVN", countrycode("South VietNam", "country.name", "cowc"))
+  expect_equal("DRV", countrycode("Democratic Republic of VietNam", "country.name", "cowc"))
+  expect_equal("DRV", countrycode("Democratic Republic of Viet Nam", "country.name", "cowc"))
+  expect_equal("DRV", countrycode("North Viet Nam", "country.name", "cowc"))
+  })
+
